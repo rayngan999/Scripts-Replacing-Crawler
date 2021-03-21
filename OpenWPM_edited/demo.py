b60b42084a3ef47dc9e2377f70f07ee9a3c93261
rayngan999 <rayngan999@gmail.com>
2021-03-20 23:22:54 -0700

    Update demo.py

--- OpenWPM_edited/demo.py
+++ OpenWPM_edited/demo.py
@@ -1,26 +1,32 @@
 from __future__ import absolute_import
 from six.moves import range
 from automation import CommandSequence, TaskManager
+
 import time
+import getFPWebsiteInfoFromJson
+import subprocess
+import sys
+import os
+import shutil
 
 # The list of sites that we wish to crawl
 NUM_BROWSERS = 1
 
-# sites = ['https://www.gamesgames.com/game/moto-x3m-5-pool-party',
-#         'https://www.google.com/',
-#         'https://www.youtube.com',
-#         'https://www.facebook.com',
-#         'https://www.baidu.com',
-#         'https://www.wikipedia.com',
-#         'https://www.qq.com',
-#         'https://www.taobao.com',
-#         'https://www.tmall.com',
-#         'https://www.amazon.com',
-#         'https://www.yahoo.com']
 
-# sites = ['https://us.trip.com/?locale=en_us','https://us.trip.com/?locale=en_us']
-sites = ['http://yahoo.com/']
-# sites = ['http://0.0.0.0:8000/canvas_fingerprinting.html', 'http://0.0.0.0:8000/eventhandlers_page.html']
+siteIndex = int(sys.argv[1])
+portNumber = int(sys.argv[2])
+obfuscator = str(sys.argv[3])
+
+sqlDir = "/home/c2/Documents/rayngan/Scripts-Replacing-Crawler/openWPM_data/" + obfuscator + "/sqlite"
+logDir = "/home/c2/Documents/rayngan/Scripts-Replacing-Crawler/openWPM_data/" + obfuscator +"/log"
+allSqlDir = "/home/c2/Documents/rayngan/Scripts-Replacing-Crawler/openWPM_data/" + obfuscator +"/all_sqlite"
+
+print("Siteindex is: " + str(siteIndex))
+print("Obfuscator is: " + obfuscator)
+print("Port number is: " + str(portNumber))
+sites = getFPWebsiteInfoFromJson.website_url(siteIndex)
+print ("Site is: " + str(sites))
+
 
 # Loads the manager preference and 3 copies of the default browser dictionaries
 manager_params, browser_params = TaskManager.load_default_params(NUM_BROWSERS)
@@ -36,32 +42,57 @@ for i in range(NUM_BROWSERS):
     browser_params[i]['headless'] = False
     browser_params[i]['save_documents'] = True
     browser_params[i]['caching_disabled'] = True
-    # browser_params[i]['profile_archive_dir'] = "./profiles"
-    browser_params[i]['profile_tar'] = "./profiles"
+    
+    
 
 # Update TaskManager configuration (use this for crawl-wide settings)
-manager_params['data_directory'] = '~/Desktop/openWPM_data'
-manager_params['log_directory'] = '~/Desktop/openWPM_data'
+if not os.path.isdir(sqlDir):
+    os.mkdir(sqlDir)
+
+manager_params['data_directory'] = sqlDir
+
+if not os.path.isdir(logDir):
+    os.mkdir(logDir)
+
+manager_params['log_directory'] = logDir
 
 # Instantiates the measurement platform
 # Commands time out by default after 60 seconds
-manager = TaskManager.TaskManager(manager_params, browser_params)
+manager = TaskManager.TaskManager(manager_params, browser_params, portNumber)
 
 # Visits the sites with all browsers simultaneously
-for site in sites:
-    command_sequence = CommandSequence.CommandSequence(site, reset=True)
+site = sites[0]
+command_sequence = CommandSequence.CommandSequence(site, reset=True)
 
-    # Start by visiting the page
-    command_sequence.get(sleep=100, timeout=100)
-    command_sequence.scroll_page()
-    command_sequence.recursive_dump_page_source()
+# Start by visiting the page
+command_sequence.get(sleep=5, timeout=30)
+command_sequence.scroll_page()
+command_sequence.recursive_dump_page_source()
 
-    # dump_profile_cookies/dump_flash_cookies closes the current tab.
-    command_sequence.dump_profile_cookies()
-    # index='**' synchronizes visits between the three browsers
-    manager.execute_command_sequence(command_sequence)
+# dump_profile_cookies/dump_flash_cookies closes the current tab.
+command_sequence.dump_profile_cookies()
+# index='**' synchronizes visits between the three browsers
+manager.execute_command_sequence(command_sequence)
 
-
-# time.sleep(100000)
 # Shuts down the browsers and waits for the data to finish logging
 manager.close()
+
+# do the clean up
+oldFileName = "crawl-data.sqlite"
+newFileName = sites[1] +".sqlite"
+newPath = "/home/c2/Documents/rayngan/Scripts-Replacing-Crawler/openWPM_data/" + obfuscator + "/all_sqlite"
+oldSql = os.path.join(sqlDir,oldFileName)
+newoSql = os.path.join(newPath,newFileName)
+
+# move the sql file together
+if os.path.exists(oldSql):
+    if not os.path.exists(newPath):
+        os.mkdir(newPath)
+    os.rename(os.path.join(sqlDir,oldFileName), os.path.join(newPath,newFileName))
+
+# delete both log directory and sql directory
+if os.path.exists(sqlDir):
+    shutil.rmtree(sqlDir)
+
+if os.path.exists(logDir):
+    shutil.rmtree(logDir)
